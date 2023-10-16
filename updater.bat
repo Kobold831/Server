7643c4beefb8eda2c81c02c6bf1ba09bfb000add
Kobold831 <146227823+Kobold831@users.noreply.github.com>
2023-10-16 21:12:20 +0900

    Add files via upload

---
+++ updater.bat
@@ -0,0 +1,49 @@
+@echo off
+
+echo アップデートファイルを確認しています．．．
+
+if exist update.zip (
+	echo ．．．OK
+	echo.
+	echo アップデートファイルを解凍しています．．．
+	echo.
+	call:tryUnzip
+	echo アップデートファイルをコピーしています．．．
+	echo.
+	call:tryCopy
+	echo 一時ファイルを削除しています．．．
+	echo.
+	call:tryDelTmp
+	call:end
+) else (
+	echo ．．．NG
+	echo.
+	echo エラーが発生したためアップデータを終了します。
+	call:end
+)
+
+:tryUnzip
+powershell -NoProfile -ExecutionPolicy Unrestricted Expand-Archive -Path update.zip -DestinationPath ./EnableGPlayWithPC\ -Force
+exit /b
+
+:tryCopy
+xcopy /e /y EnableGPlayWithPC .."\.."\
+echo.
+exit /b
+
+:tryDelTmp
+del /s /q update.zip
+rd /s /q EnableGPlayWithPC
+exit /b
+
+:end
+echo.
+echo アップデータは正常に終了しました。
+echo.
+echo 続行したあと、EnableGPlayWithPCは自動起動します。
+echo.
+echo 続行するには何かキーを押してください．．．
+pause > NUL
+start EnableGPlayWithPC.exe
+del /f "%~dp0%~nx0"
+exit
