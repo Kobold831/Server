6e9d44ab86289bf291ed74cd616b5684dd000485
Kobold831 <146227823+Kobold831@users.noreply.github.com>
2023-11-15 00:57:39 +0900

    Fixes

--- updater.bat
+++ updater.bat
@@ -14,12 +14,12 @@ if exist update.zip (
 	echo 一時ファイルを削除しています．．．
 	echo.
 	call:tryDelTmp
-	call:end
+	call:success
 ) else (
 	echo ．．．NG
 	echo.
 	echo エラーが発生したためアップデータを終了します。
-	call:end
+	call:failure
 )
 
 :tryUnzip
@@ -36,14 +36,30 @@ del /s /q update.zip
 rd /s /q EnableGPlayWithPC
 exit /b
 
-:end
+:success
 echo.
 echo アップデータは正常に終了しました。
 echo.
 echo 続行したあと、EnableGPlayWithPCは自動起動します。
 echo.
-echo 続行するには何かキーを押してください．．．
-pause > NUL
-start ダブルクリックして起動.bat
+for /l %%a in (5,-1,1) do (
+  echo 続行するには何かキーを押してください．．．動作がない場合%%a秒後に自動起動します．．．
+  timeout /t 1 > nul
+)
+start bin\ILMerge\EnableGPlayWithPC.exe
+del /f "%~dp0%~nx0"
+exit
+
+:failure
+echo.
+echo アップデータはエラーで終了しました。
+echo.
+echo 続行したあと、EnableGPlayWithPCは自動起動します。
+echo.
+for /l %%a in (5,-1,1) do (
+  echo 続行するには何かキーを押してください．．．動作がない場合%%a秒後に自動起動します．．．
+  timeout /t 1 > nul
+)
+start bin\ILMerge\EnableGPlayWithPC.exe
 del /f "%~dp0%~nx0"
 exit
